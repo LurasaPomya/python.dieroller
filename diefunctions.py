e0f05b134d7c18dc24687f61a08a6bff4935089f
James McGrew <jemcgrew@gmail.com>
2015-04-26 22:46:56 -0400

    Changed roll.py to rollcli.py as it will be for CLI only. Also improved functions to be generic. They do not print to stdout anymore. This allows for them to be called for different interfaces.

--- diefunctions.py
+++ diefunctions.py
@@ -5,25 +5,20 @@ def roll(sides,count):
     count = int(count)
     n = 0
     total = 0
+
+    results = []
     while n < count:
         roll = randint(1,sides)
-        print "Roll {} was {}".format(n + 1,roll)
+        results.append(roll)
         total += roll
         n += 1
-        print "Total: {}".format(total)
-    raw_input("Press Enter to return to menu...")
-    return
+    return (results,total)
 def skillCheck(modifier,prof):
     modifier = int(modifier)
     prof = int(prof)
     if prof != 0:
         modifier += prof
-        roll = randint(1,20)
-    if roll == 20:
-        print "Critical Success! Modified Roll: {}".format(roll + modifier)
-    elif roll == 1:
-        print "Critical Failure! Modified Roll: {}".format(roll + modifier)
-    else:
-        print "Rolled {}, Modified to {}".format(roll,roll + modifier)
-    raw_input("Press Enter to return to the menu...")
-    return
+    
+    roll = randint(1,20)
+    modroll = roll + modifier
+    return (roll, modroll)
--- rollcli.py
+++ rollcli.py
@@ -1,6 +1,10 @@
 import os
 import diefunctions
 
+def wfi():
+    raw_input("Press Enter to Continue...")
+    return
+
 while True:
     os.system('clear')
     print "Dec's Dice Roller! Please choose:"
@@ -17,11 +21,26 @@ while True:
     elif selection == 1:
         s = raw_input("How many sides: ")
         c = raw_input("How many to roll: ")
-        diefunctions.roll(s,c)
+        results = diefunctions.roll(s,c)
+        rolls = results[0]
+        total = results[1]
+        c = 1
+        for roll in rolls:
+            print "Roll {} was {}".format(c,roll)
+            c += 1
+        print "Total was: {}".format(total)
+        wfi()
     elif selection == 2:
         m = raw_input("Modofier to roll(w/o Prof): ")
         p = raw_input("Prof. Modifier: ")
-        diefunctions.skillCheck(m,p)
+        results = diefunctions.skillCheck(m,p)
+        if results[0] == 20:
+            print "Critical Success!"
+        elif results[0] == 1:
+            print "Critical Failure!"
+        else:
+            print "You rolled {}! Modded to {}".format(results[0],results[1])
+            wfi()
     else:
         print "Invalid Selection!"
 
