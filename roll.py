c90c1e99b459697901c0260812df797a772b531c
James McGrew <jemcgrew@gmail.com>
2015-04-26 20:30:17 -0400

    Cleaned up and improved output and input

--- roll.py
+++ roll.py
@@ -3,16 +3,21 @@ from random import randint
 sides = 6
 count = 1
 
-while sides != 0:
+while True:
     print "Dec's Dice Roller! Enter 0 as sides to exit!"
     sides = raw_input("Enter the Number of Sides:")
-    count = raw_input("How many to roll:")
     sides = int(sides)
+    #Checks to see if we should break and stop asking
+    if int(sides) == 0:
+        break
+    count = raw_input("How many to roll:")
+    count = int(count)
+    
     n = 0
     total = 0
-    while n < sides:
+    while n < count:
         roll = randint(1,sides)
-        print "Roll {} was {}".format(n,roll)
+        print "Roll {} was {}".format(n + 1,roll)
         total += roll
         n += 1
     print total;
